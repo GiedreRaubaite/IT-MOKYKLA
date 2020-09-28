1d65389b52469906df27098f48422d17168e2401
GiedreRaubaite <58292729+GiedreRaubaite@users.noreply.github.com>
2020-09-28 13:39:32 +0200

    Update index.js

--- Lesson 8/js/index.js
+++ Lesson 8/js/index.js
@@ -1,55 +1,60 @@
-var ToDo = [{ name: "ToDo", tasks: [] }];
-var Doing = [{ name: "Doing", tasks: [] }];
-var Done = [{ name: "Done", tasks: [] }];
-var boardData = [];
+let boardData = [{ name: 'ToDo', tasks: [] }, { name: 'Doing', tasks: [] }, { name: 'Done', tasks: [] }];
 /* Add card */
-var addCardButtons = document.getElementsByClassName("addCard");
-Array.from(addCardButtons).forEach(function (button) {
+const addCardButtons = document.getElementsByClassName("addCard");
+[...addCardButtons].forEach(function (button) {
     button.addEventListener('click', function (evt) {
-        var text =
+        let text =
             `<li>
                <i class="delete-button fas fa-times"></i><input type = "text" class="task"><i class="left fas fa-angle-left"></i> <i class="right fas fa-angle-right"></i> 
         </li> `
-        evt.target.parentNode.parentNode.children[1].insertAdjacentHTML("beforeend", text);
-        var lastAddedInput = evt.target.parentNode.parentNode.children[1].lastElementChild.children[1];
-        addNewTask(lastAddedInput);
+        evt.target.closest('.content').children[1].insertAdjacentHTML("beforeend", text);
+        let lastAddedInput = evt.target.closest('.content').children[1].lastElementChild.children[1];
+        let whichColumn = evt.target.closest('.content').id;
+        let taskID = Math.random();
+        let taskText = lastAddedInput.value;
+        lastAddedInput.parentNode.setAttribute("id", taskID.toString());
+        boardData.find(FindArray, whichColumn).tasks.push({
+            task: taskText,
+            id: taskID
+        });
+        console.log(boardData);
         updateAddCardText()
     });
 });
 /* Move card to left column */
 document.body.addEventListener('click', function (evt) {
-    var toWhichColumn;
+    let targetColumn;
     if (evt.target.className === 'left fas fa-angle-left') {
-        var fromWhichColumn = evt.target.parentNode.parentNode.parentNode.id;
-        fromWhichColumn === 'Doing' ? toWhichColumn = 'ToDo'
-            : fromWhichColumn === 'Done' ? toWhichColumn = 'Doing'
-                : toWhichColumn = 'ToDo';
-        changeCardPosition(evt, toWhichColumn, fromWhichColumn);
-        updateBoardData(evt, toWhichColumn, fromWhichColumn);
+        let lastColumn = evt.target.closest('.content').id;
+        lastColumn === 'Doing' ? targetColumn = 'ToDo'
+            : lastColumn === 'Done' ? targetColumn = 'Doing'
+                : targetColumn = 'ToDo';
+        changeCardPosition(evt, targetColumn, lastColumn);
+        updateBoardData(evt, targetColumn, lastColumn);
         updateAddCardText();
     };
-/* Move card to right column */
+    /* Move card to right column */
     if (evt.target.className === 'right fas fa-angle-right') {
-        var fromWhichColumn = evt.target.parentNode.parentNode.parentNode.id;
-        fromWhichColumn === 'Doing' ? toWhichColumn = 'Done'
-            : fromWhichColumn === 'ToDo' ? toWhichColumn = 'Doing'
-                : toWhichColumn = 'Done';
-        changeCardPosition(evt, toWhichColumn, fromWhichColumn);
-        updateBoardData(evt, toWhichColumn, fromWhichColumn);
+        let lastColumn = evt.target.closest(`.content`).id;
+        lastColumn === 'Doing' ? targetColumn = 'Done'
+            : lastColumn === 'ToDo' ? targetColumn = 'Doing'
+                : targetColumn = 'Done';
+        changeCardPosition(evt, targetColumn, lastColumn);
+        updateBoardData(evt, targetColumn, lastColumn);
         updateAddCardText();
     };
 });
 /* Delete card */
 document.body.addEventListener('click', function (evt) {
     if (evt.target.className === 'delete-button fas fa-times') {
-        boardData.forEach(function (element, i) {
-            find = element.tasks.findIndex(findElement, evt.target.parentNode.id);
-            if (find >= 0) {
-                element.tasks.splice(find, 1);
-                updateAddCardText();
-                evt.target.parentNode.parentNode.removeChild(evt.target.parentNode);
-            };
+        boardData.forEach(function (element) {
+            dltEl = element.tasks.find(findElement, evt.target.parentNode.id);
+            boardData = boardData.map((element) => {
+                return { ...element, tasks: element.tasks.filter((el) => el !== dltEl) }
+            })
         });
+        updateAddCardText();
+        evt.target.parentNode.parentNode.removeChild(evt.target.parentNode);
     }
 });
 /* FocusOut, FocusIn events to note changes, edits in card inputs */
@@ -68,66 +73,54 @@ document.body.addEventListener("focusin", function (evt) {
         });
     };
 }, false);
-/*Edit existing Task in boardData array */ 
+/*Edit existing Task in boardData array */
 function editTask(evt) {
     if (boardData.length > 0) {
         boardData.forEach(function (element) {
-            var find = element.tasks.find(findElement, evt.target.parentNode.id);
-            if (find) {
-                find.task = evt.target.value;
+            var editedEl = element.tasks.find(findElement, evt.target.parentNode.id);
+            if (editedEl) {
+                editedEl.task = evt.target.value;
             }
         });
     }
 };
-/*Add new task in boardData array */
-function addNewTask(evt) {
-    var whichArray = eval(evt.parentNode.parentNode.parentNode.id);
-    var taskID = Math.random();
-    var TASKtext = evt.value;
-    evt.parentNode.setAttribute("id", taskID.toString());
-    whichArray[0].tasks.push({
-        task: TASKtext,
-        id: taskID
-    });
-    boardData = [...ToDo, ...Doing, ...Done];
-    console.log(boardData);
-};
 /*Update boardData array when the elements are moved from one column to another */
 function updateBoardData(evt, toWhichColumn, fromWhichColumn) {
-    fromWhichColumn = eval(fromWhichColumn);
-    toWhichColumn = eval(toWhichColumn);
-    var find = fromWhichColumn[0].tasks.find(findElement, evt.target.parentNode.id);
-    if (find) {
-        toWhichColumn[0].tasks.push({
-            task: find.task,
-            id: find.id
+    let movedEl = boardData.find(FindArray, fromWhichColumn).tasks.find(findElement, evt.target.parentNode.id);
+    if (movedEl) {
+        boardData.find(FindArray, toWhichColumn).tasks.push({
+            task: movedEl.task,
+            id: movedEl.id
         });
-        var indexOfFind = fromWhichColumn[0].tasks.indexOf(find)
-        fromWhichColumn[0].tasks.splice(indexOfFind, 1);
+        boardData = boardData.map((element) => {
+            return { ...element, tasks: element.tasks.filter((el) => el !== movedEl) }
+        })
     };
 };
 /* Find testing function */
 function findElement(element) {
     return ((element.id).toString()) === this.toString();
 };
+function FindArray(element) {
+    return (element.name) === this.toString();
+};
 /* Update Addcard text according boardData data */
 function updateAddCardText() {
     if (boardData.length > 0) {
-        document.getElementById("AddCard1").innerHTML = (ToDo[0].tasks.length > 0) ?"Add another card" : "Add card"
-        document.getElementById("AddCard2").innerHTML = (Doing[0].tasks.length > 0) ?"Add another card" : "Add card"
-        document.getElementById("AddCard3").innerHTML = (Done[0].tasks.length > 0) ?"Add another card" : "Add card"           
+        document.getElementById("AddCard1").innerHTML = (boardData[0].tasks.length > 0) ? "Add another card" : "Add card"
+        document.getElementById("AddCard2").innerHTML = (boardData[1].tasks.length > 0) ? "Add another card" : "Add card"
+        document.getElementById("AddCard3").innerHTML = (boardData[2].tasks.length > 0) ? "Add another card" : "Add card"
     }
 }
-function changeCardPosition(evt, toWhichColumn, fromWhichColumn) {
-    fromWhichColumn = eval(fromWhichColumn);
-    var find = fromWhichColumn[0].tasks.find(findElement, evt.target.parentNode.id);
-    evt.target.parentNode.parentNode.removeChild(evt.target.parentNode);
+function changeCardPosition(evt, targetColumn, lastColumn) {
+    let movedEl = boardData.find(FindArray, lastColumn).tasks.find(findElement, evt.target.parentNode.id);
+    evt.target.closest('.list').removeChild(evt.target.parentNode);
     value = evt.target.parentNode.children[1].value;
     text =
-        `<li id = "${find.id}">
+        `<li id = "${movedEl.id}">
         <i class="delete-button fas fa-times"></i><input type = "text" class="task" value = "${value}"><i class="left fas fa-angle-left"></i> <i class="right fas fa-angle-right"></i> 
  </li> `
-    var targetColumn = document.getElementById(toWhichColumn);
-    targetColumn.children[1].insertAdjacentHTML("beforeend", text);
+    let targetColumnDiv = document.getElementById(targetColumn);
+    targetColumnDiv.children[1].insertAdjacentHTML("beforeend", text);
 }
 
