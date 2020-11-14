242cd45c1363e3e5fd9a03a8b517bcd639d3c49f
GiedreRaubaite <58292729+GiedreRaubaite@users.noreply.github.com>
2020-11-14 13:52:21 +0100

    style update
    style update

--- Lesson 10/js/index.js
+++ Lesson 10/js/index.js
@@ -1,51 +1,65 @@
 
-$(window).on("load", function(){
+$(window).on("load", function () {
     $("#preloader").delay(350).fadeOut('slow');
 });
 
-$(document).ready(function() {
-const section = document.querySelector('#counting');
-let hasEntered = false;
-window.addEventListener('scroll', (e) => {
-    const shouldAnimate = (window.scrollY + window.innerHeight) >= section.offsetTop;
-    if (shouldAnimate && !hasEntered) {
-        hasEntered = true;
-        $('.numbers').each(function () {
-            $(this).prop('Counter', 0).animate({
-                Counter: $(this).text()
-            }, {
-                duration: 4000,
-                easing: 'swing',
-                step: function (now) {
-                    $(this).text(Math.ceil(now));
-                }
+$(document).ready(function () {
+    const section = document.querySelector('#counting');
+    let hasEntered = false;
+    window.addEventListener('scroll', (e) => {
+        const shouldAnimate = (window.scrollY + window.innerHeight) >= section.offsetTop;
+        if (shouldAnimate && !hasEntered) {
+            hasEntered = true;
+            $('.numbers').each(function () {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
             });
+        }
+    });
+    $('#navigationButton').on('click', function () {
+        $("#leftBlock").toggleClass("d-inline");
+    });
+    $('#closeButton').on('click', function () {
+        $("#leftBlock").removeClass("d-inline").addClass("d-none");
+    });
+    $(".list-inline-item").on('click', function () {
+        $(".list-inline-item").each(function () {
+            $(this).removeClass("current");
+            $(this).addClass("text-secondary");
         });
-    }
-});
-$('#navigationButton').on('click', function () {
-    $("#leftBlock").toggleClass("d-inline");
-});
-$('#closeButton').on('click', function () {
-    $("#leftBlock").removeClass("d-inline").addClass("d-none");
-});
-$(".list-inline-item").on('click', function () {
-    $(".list-inline-item").each(function () {
-        $(this).removeClass("current");
-        $(this).addClass("text-secondary");
+        $(this).toggleClass("current");
+        $(this).removeClass("text-secondary");
+    })
+    $(window).on('activate.bs.scrollspy', function () {
+        $(".nav-link").each(function () {
+            if ($(this).hasClass("active")) {
+                $(this).children("span").css("background", "#ff5959");
+            }
+            else {
+                $(this).children("span").css("background", "#4a63e7");
+            }
+        });
     });
-    $(this).toggleClass("current");
-    $(this).removeClass("text-secondary");
-})
-$(window).on('activate.bs.scrollspy', function () {
-    $(".nav-link").each(function () {
-        if ($(this).hasClass("active")) {
-            $(this).children("span").css("background", "#ff5959");
-        }
-        else {
-            $(this).children("span").css("background", "#4a63e7");
-        }
+    $("#works li").click(function () {
+        const customType = $(this).data('filter');
+        $(".works").css({
+            "opacity": "0",
+        });
+        $(customType).css({
+            "opacity": "1",
+        });
     });
-});
+    $("#loadMoreButton").click(function () {
+        let text = $(this).text();
+        $("#loadMore").toggleClass("d-none");
+        $(this).text(text == " Load More " ? " Show Less " : " Load More ");
+    })
 });
 
