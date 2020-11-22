a74d1ec8b97b78fa7ca88ec48617cf44f63247c3
GiedreRaubaite <58292729+GiedreRaubaite@users.noreply.github.com>
2020-11-22 13:45:28 +0100

    CV website
    CV website

---
+++ Lesson 10/js/counting.js
@@ -0,0 +1,22 @@
+
+function Counting(){
+    const section = document.querySelector('#counting');
+    let hasEntered = false;
+window.addEventListener('scroll', (e) => {
+        const shouldAnimate = (window.scrollY + window.innerHeight) >= section.offsetTop;
+        if (shouldAnimate && !hasEntered) {
+            hasEntered = true;
+            $('.numbers').each(function () {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+            });
+        }
+    });
+};
--- Lesson 10/js/index.js
+++ Lesson 10/js/index.js
@@ -1,65 +1,94 @@
-
-$(window).on("load", function () {
-    $("#preloader").delay(350).fadeOut('slow');
-});
-
 $(document).ready(function () {
-    const section = document.querySelector('#counting');
-    let hasEntered = false;
-    window.addEventListener('scroll', (e) => {
-        const shouldAnimate = (window.scrollY + window.innerHeight) >= section.offsetTop;
-        if (shouldAnimate && !hasEntered) {
-            hasEntered = true;
-            $('.numbers').each(function () {
-                $(this).prop('Counter', 0).animate({
-                    Counter: $(this).text()
-                }, {
-                    duration: 4000,
-                    easing: 'swing',
-                    step: function (now) {
-                        $(this).text(Math.ceil(now));
-                    }
-                });
-            });
-        }
-    });
-    $('#navigationButton').on('click', function () {
-        $("#leftBlock").toggleClass("d-inline");
-    });
-    $('#closeButton').on('click', function () {
-        $("#leftBlock").removeClass("d-inline").addClass("d-none");
-    });
-    $(".list-inline-item").on('click', function () {
-        $(".list-inline-item").each(function () {
+    Counting();
+    MobileNavigationClickEvents();
+    FormCustomMessages();
+    Scrollspy();
+    Isotope();
+    $("#preloader").delay(350).fadeOut('slow');
+    $("#navbarWorksLarge .list-inline-item").on('click', function () {
+       $(".list-inline-item").each(function () {
             $(this).removeClass("current");
             $(this).addClass("text-secondary");
         });
         $(this).toggleClass("current");
         $(this).removeClass("text-secondary");
-    })
-    $(window).on('activate.bs.scrollspy', function () {
-        $(".nav-link").each(function () {
-            if ($(this).hasClass("active")) {
-                $(this).children("span").css("background", "#ff5959");
-            }
-            else {
-                $(this).children("span").css("background", "#4a63e7");
-            }
-        });
     });
-    $("#works li").click(function () {
+    $("#works li").on("click", function () {
         const customType = $(this).data('filter');
-        $(".works").css({
-            "opacity": "0",
-        });
-        $(customType).css({
-            "opacity": "1",
+        $("#works-block").isotope({
+            filter: customType
         });
     });
-    $("#loadMoreButton").click(function () {
+    $("#loadMoreButton").on("click", function () {
         let text = $(this).text();
         $("#loadMore").toggleClass("d-none");
         $(this).text(text == " Load More " ? " Show Less " : " Load More ");
+        $(".current").trigger("click");
     })
-});
+    $("#navigation li").on('click', function () {
+        if ($(document).width() < 768) {
+            $("#leftBlock").removeClass("d-inline").addClass("d-none");
+        }
+        else {
+            return;
+        }
+    });
 
+    function FormCustomMessages(){
+    $("#submitButton").on("click", function () {
+        var name = document.getElementById('name').value;
+        if (name == "") {
+            document.querySelector('.status').innerHTML = '<span class="text-danger font-weight-bold">Name cannot be empty!! </span>';
+            return false;
+        }
+        var email = document.getElementById('email').value;
+        if (email == "") {
+            document.querySelector('.status').innerHTML = '<span class="text-danger font-weight-bold">E-mail cannot be empty!! </span>';
+            return false;
+        } else {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (!re.test(email)) {
+                document.querySelector('.status').innerHTML = '<span class="text-danger font-weight-bold">Email format invalid!! </span>';
+                return false;
+            }
+        }
+        var subject = document.getElementById('subject').value;
+        if (subject == "") {
+            document.querySelector('.status').innerHTML = '<span class="text-danger font-weight-bold">Subject cannot be empty!! </span>';
+            return false;
+        }
+        var message = document.getElementById('message').value;
+        if (message == "") {
+            document.querySelector('.status').innerHTML = '<span class="text-danger font-weight-bold">Message cannot be empty!! </span>';
+            return false;
+        }
+        document.querySelector('.status').innerHTML = '<span class="text-success font-weight-bold">Sending.... </span>';
+    });
+};   
+    function Isotope(){
+    $("#works-block").isotope({
+        itemSelector: '.works',
+        layoutMode: 'fitRows'
+    });
+};
+    function MobileNavigationClickEvents() {
+        $('#navigationButton').on('click', function () {
+        $("#leftBlock").toggleClass("d-inline");
+        });
+        $('#closeButton').on('click', function () {
+            $("#leftBlock").removeClass("d-inline").addClass("d-none");
+        });
+    }
+    function Scrollspy(){
+        $(window).on('activate.bs.scrollspy', function () {
+            $(".nav-link").each(function () {
+                if ($(this).hasClass("active")) {
+                    $(this).children("span").css("background", "#ff5959");
+                }
+                else {
+                    $(this).children("span").css("background", "#4a63e7");
+                }
+            });
+        });
+    };
+});
