05bc56fdd4b22874daa8813b35fa27227b5c426a
GiedreRaubaite <58292729+GiedreRaubaite@users.noreply.github.com>
2021-01-12 22:17:44 +0100

    refactored code Shopping Basket
    refactored code Shopping Basket
    unabled trash buttons

--- Lesson 9/js/index.js
+++ Lesson 9/js/index.js
@@ -1,140 +1,171 @@
-/*--------------get things from localStorage------------------ *\
-/*how many items in the shoppingcart, menubar */
-var totalShoppingItems = localStorage.getItem('totalItems');
-if (totalShoppingItems) {
-  totalShoppingItems == 1 ? document.getElementById("countingText").innerHTML = `<p>There is 1 item in your <i class='fas fa-shopping-cart'></i> </p>` : document.getElementById("countingText").innerHTML = `<p>There are ${totalShoppingItems} items in your <i class='fas fa-shopping-cart'></i> </p>`;
-};
-/*checkout sum in the footer */
-var checkoutSum = JSON.parse(localStorage.getItem('checkoutSum'));
-if (checkoutSum) {
-  document.getElementById("subtotal").innerHTML = checkoutSum;
+let shoppingItems = localStorage.getItem('totalItems');
+if (shoppingItems) {
+    updateShoppingCartText(shoppingItems);
 }
-/*values for all quantity inputs */
-var quantityEachEl = JSON.parse(localStorage.getItem('quantity'));
+let checkOutSum = JSON.parse(localStorage.getItem('checkOutHTML'));
+if (checkOutSum) {
+    document.getElementById("subtotal").innerHTML = checkOutSum;
+}
+let quantityEachEl = JSON.parse(localStorage.getItem('quantity'));
 if (quantityEachEl) {
-  document.querySelectorAll(".quantity").forEach(function (element, i = 0) {
-    element.value = quantityEachEl[i];
-    i++;
-  });
+    document.querySelectorAll(".quantity").forEach(function(element, i = 0) {
+        element.value = quantityEachEl[i];
+        i++;
+    });
 };
-/*---------------***************************------------------*\
-/*click shopping cart icon */
-var shoppingCartBtn = document.querySelectorAll(".shopButton");
-shoppingCartBtn.forEach(function (element) {
-  element.addEventListener('click', function (e) {
-    addItem(e);
-  });
-});
-/*click trash button icon */
-document.getElementById("confirm").addEventListener('click', function (e) {
-  var TrashBtn = document.querySelectorAll(".trashButton");
-  TrashBtn.forEach(function (element) {
-    if (element.getAttribute("data-clicked") == "true") {
-      removeItem(element);
-    }
-  })
-  $('#confirmation').modal('hide');
-});
-/*additem to shoppingcart, reduce quantity, count total sum */
+let notDisabledButtons = JSON.parse(localStorage.getItem('notDisabledButtons'));
+if (notDisabledButtons && (notDisabledButtons.length > 0)) {
+    notDisabledTrashButtonsArray = [];
+    notDisabledButtons.forEach(function(element) {
+        document.getElementById(element).disabled = false;
+        notDisabledTrashButtonsArray.push(element);
+    })
+} else {
+    notDisabledTrashButtonsArray = [];
+}
+
+function initializeEventliteners() {
+    const shoppingCartButtons = document.querySelectorAll(".shopButton");
+    $(shoppingCartButtons).children().tooltip();
+    shoppingCartButtons.forEach(function(element) {
+        element.addEventListener('click', function(e) {
+            addItem(e);
+        });
+    });
+    document.getElementById("confirmItemRemove").addEventListener('click', function(e) {
+        $('#confirmation').modal('hide');
+        removeItem(findItemToRemove());
+    });
+    const trashButtons = document.querySelectorAll(".trashButton");
+    trashButtons.forEach(function(element) {
+        element.addEventListener('click', function(e) {
+            e.currentTarget.setAttribute("data-clicked", true);
+        });
+    });
+    document.body.addEventListener('click', function(e) {
+        if (e.target.classList.contains("CheckOut")) {
+            $('#popuptext').html("Sorry, I know you're thirsty... <br> But this shop is not open now :) ");
+            $('#popup').modal("show");
+        };
+    })
+};
+
+initializeEventliteners();
+
 function addItem(e) {
-  var quantity = parseInt(e.target.closest('tr').childNodes[5].childNodes[1].value);
-  var reducedQuantity = quantity - 1
-  if (quantity === 0) {
+    const quantityArr = [];
+    const productQuantityElement = e.target.closest('tr').childNodes[5].childNodes[1];
+    const trashButtonToEnable = e.target.closest('tr').childNodes[7].childNodes[1].children[1];
+    const remainingProductQuantity = parseInt(productQuantityElement.value);
+    const reducedQuantity = remainingProductQuantity - 1
+    $(trashButtonToEnable).children().tooltip();
+    if (remainingProductQuantity === 0) {
+        return showNoMoreItemsModal();
+    } else {
+        productQuantityElement.value = reducedQuantity;
+        document.querySelectorAll(".quantity").forEach(function(element) {
+            quantityArr.push(element.value);
+            localStorage.setItem('quantity', JSON.stringify(quantityArr));
+        });
+    };
+    toggleTrashButtonEnableDisable(trashButtonToEnable, false);
+    updateShoppingCartQuantity();
+    updateSubtotal(e.target);
+}
+
+function removeItem(e) {
+    const remainingQuantity = parseInt(e.closest('tr').childNodes[5].childNodes[1].value);
+    const trashButtonToDisable = e.closest('tr').childNodes[7].childNodes[1].children[1];
+    if (remainingQuantity === 19) {
+        toggleTrashButtonEnableDisable(trashButtonToDisable);
+    };
+    const increasedQuantity = remainingQuantity + 1;
+    e.closest('tr').childNodes[5].childNodes[1].value = increasedQuantity;
+    quantityArr = [];
+    document.querySelectorAll(".quantity").forEach(function(element) {
+        quantityArr.push(element.value);
+        localStorage.setItem('quantity', JSON.stringify(quantityArr));
+    });
+    updateShoppingCartQuantity(false);
+    updateSubtotal(e, false);
+}
+
+function findItemToRemove(e) {
+    const trashButtons = document.querySelectorAll(".trashButton");
+    let itemToRemove = null;
+    trashButtons.forEach(function(element) {
+        if (element.getAttribute("data-clicked") == "true") {
+            itemToRemove = element;
+            element.setAttribute("data-clicked", false);
+        }
+    });
+    return itemToRemove;
+}
+
+function updateShoppingCartText(shoppingItems) {
+    let itemCountMessage = shoppingItems == 1 ? `is 1 item` : `are ${shoppingItems} items`;
+    document.getElementById("countingText").innerHTML = `<p>There ${itemCountMessage} in your <i class='fas fa-shopping-cart'></i> </p>`;
+}
+
+function updateShoppingCartQuantity(isAdd = true) {
+    let updateValue = 1;
+    if (!isAdd) {
+        updateValue = -1;
+    }
+    if (localStorage.hasOwnProperty('totalItems')) {
+        const totalItems = localStorage.getItem('totalItems');
+        if (!isAdd && totalItems == 0) return;
+        const newQuantity = parseInt(totalItems) + updateValue;
+        updateShoppingCartText(newQuantity);
+        localStorage.setItem('totalItems', (newQuantity));
+    } else {
+        updateShoppingCartText(1);
+        localStorage.setItem('totalItems', 1);
+    };
+}
+
+function showNoMoreItemsModal() {
     $('#popuptext').html("Sorry... <br> There are no items left");
     $('#popup').modal("show");
-    return;
-  }
-  else {
-    e.target.closest('tr').childNodes[5].childNodes[1].value = reducedQuantity;
-    quantityArr = [];
-    document.querySelectorAll(".quantity").forEach(function (element) {
-      quantityArr.push(element.value);
-      localStorage.setItem('quantity', JSON.stringify(quantityArr));
-    });
-  };
-  /*update text in the shopping cart */
-  if (localStorage.hasOwnProperty('totalItems')) {
-    var totalItems = localStorage.getItem('totalItems');
-    document.getElementById("countingText").innerHTML = `<p>There are ${parseFloat(totalItems) + 1} items in your <i class='fas fa-shopping-cart'></i> </p>`;
-    localStorage.setItem('totalItems', (parseFloat(totalItems) + 1))
-  }
-  else {
-    document.getElementById("countingText").innerHTML = `<p>There is 1 item in your <i class='fas fa-shopping-cart'></i> </p>`;
-    localStorage.setItem('totalItems', 1)
-  };
-  /*update subtotal price in footer Subtotal  */
-  var price = parseFloat(e.target.closest('tr').childNodes[3].innerText);
-  if (localStorage.hasOwnProperty('subtotal')) {
-    var subtotalPrice = localStorage.getItem('subtotal');
-    subtotalPrice = parseFloat(subtotalPrice) + parseFloat(price);
-  }
-  else {
-     var subtotalPrice = price;
-  }
-  localStorage.setItem('subtotal', subtotalPrice);
-  var checkOutText = `<h4>Subtotal:</h4>
-   <h1>${subtotalPrice.toFixed(2)} Eur</h1> <button class="CheckOut btn btn-dark mb-4 btn-lg pl-5 pr-5">Checkout</a><h1 >`;
-  document.getElementById("subtotal").innerHTML = checkOutText;
-  localStorage.setItem('checkoutSum', JSON.stringify(checkOutText));
 }
-/* remove item, increase quantity, count total */
-function removeItem(e) {
-  e.removeAttribute("data-clicked");
-  var quantity = parseInt(e.closest('tr').childNodes[5].childNodes[1].value);
-  if (quantity === 20) {
-    $('#popuptext').html("<br> This item is not in your shopping cart");
-    $('#popup').modal("show");
-    return;
-  };
-  var increasedQuantity = quantity + 1;
-  e.closest('tr').childNodes[5].childNodes[1].value = increasedQuantity;
-  quantityArr = [];
-  document.querySelectorAll(".quantity").forEach(function (element) {
-    quantityArr.push(element.value);
-    localStorage.setItem('quantity', JSON.stringify(quantityArr));
-  });
-  /*text in the shopping cart */
-  if (localStorage.hasOwnProperty('totalItems')) {
-    var totalItems = localStorage.getItem('totalItems');
-    document.getElementById("countingText").innerHTML = `<p>There are ${parseFloat(totalItems) - 1} items in your <i class='fas fa-shopping-cart'></i> </p>`;
-    localStorage.setItem('totalItems', (parseFloat(totalItems) - 1));
-  }
-  else {
-    document.getElementById("countingText").innerHTML = `<p>There is 1 item in your <i class='fas fa-shopping-cart'></i> </p>`;
-    localStorage.setItem('totalItems', 1);
-  };
-  /*update total price in the footer Subtotal  */
-  var price = parseFloat(e.closest('tr').childNodes[3].innerText);
-  var subtotalPrice = localStorage.getItem('subtotal');
-  subtotalPrice = parseFloat(subtotalPrice) - parseFloat(price);
-  var checkOutText = `<h4>Subtotal:</h4>
-    <h1>${subtotalPrice.toFixed(2)} Eur</h1> <button class="CheckOut btn btn-dark mb-4 btn-lg pl-5 pr-5">Checkout</a><h1 >`;
-  document.getElementById("subtotal").innerHTML = checkOutText;
-  localStorage.setItem('subtotal', subtotalPrice);
-  localStorage.setItem('checkoutSum', JSON.stringify(checkOutText));
-};
-/*-------------------------------------------------------------------------------*/
-/* set attribute to clicked button */
-var TrashBtn = document.querySelectorAll(".trashButton");
-TrashBtn.forEach(function (element) {
-  element.addEventListener('click', function (e) {
-    e.currentTarget.setAttribute("data-clicked", true);
-  });
-});
-/*checkout button*/
-document.body.addEventListener('click', function(e){
-  if (e.target.classList.contains("CheckOut")){
-    $('#popuptext').html("Sorry, I know you're thirsty... <br> But this shop is not open now :) ");
-    $('#popup').modal("show");
-  };
-})
 
-/*tooltips*/
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-});
+function updateSubtotal(clickedButton, isAdd = true) {
+    let updateValue = null;
+    let subtotal = 0;
+    let price = parseFloat(clickedButton.closest('tr').childNodes[3].innerText);
+    if (!isAdd) {
+        price = -price;
+    }
+    if (localStorage.hasOwnProperty('subtotal')) {
+        subtotal = parseFloat(localStorage.getItem('subtotal')) + parseFloat(price);
+    } else {
+        subtotal = price;
+    }
+    localStorage.setItem('subtotal', subtotal);
+    updateSubtotalHTML(subtotal);
+}
+
+function updateSubtotalHTML(subtotal) {
+    var checkOutText = `<h4>Subtotal:</h4>
+  <h1>${subtotal.toFixed(2)} Eur</h1> <button class="CheckOut btn btn-dark mb-4 btn-lg pl-5 pr-5">Checkout</a>`;
+    document.getElementById("subtotal").innerHTML = checkOutText;
+    localStorage.setItem('checkOutHTML', JSON.stringify(checkOutText));
+}
+
+function toggleTrashButtonEnableDisable(trashButton, disabled = true) {
+    if (!disabled) {
+        trashButton.disabled = false;
+        $(trashButton).children().tooltip();
+        notDisabledTrashButtonsArray.push(trashButton.id);
+        localStorage.setItem('notDisabledButtons', JSON.stringify(notDisabledTrashButtonsArray));
+    } else {
+        trashButton.disabled = true;
+        const index = notDisabledTrashButtonsArray.indexOf(trashButton.id);
+        notDisabledTrashButtonsArray.splice(index, 1);
+        $(trashButton).children().tooltip("disable");
+        localStorage.setItem('notDisabledButtons', JSON.stringify(notDisabledTrashButtonsArray));
+
+    }
 
 
-
-
-
+}
